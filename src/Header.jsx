c664ad47d2f5cdc873d8b81358eb0adeda82df89
FrancesHitchcock <95710313+FrancesHitchcock@users.noreply.github.com>
2023-03-04 07:25:37 +0000

    Relocation of images

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header(){
     return(
         <div className="header">
-            <img src="./public/images/earth-icon.png"/><h1>my travel journal.</h1>
+            <img src="/earth-icon.png"/><h1>my travel journal.</h1>
         </div>
     )
 }
